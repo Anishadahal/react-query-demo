2e6bff91541de7b435447cc22b3de6cc7d151bef
Anisha Dahal <anisha.dahal@webo.digital>
2022-06-29 17:50:44 +0545

    ssr fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,34 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { dehydrate, QueryClient, useQuery } from "react-query";
-import { Genres, MovieData } from "../models/movies";
+import { Genre, Genres, MovieData } from "../models/movies";
 import { getMovieByGenre, getMovieData, searchMoviesByTitle } from "../services/fetch-data";
 import Header from "../templates/header";
 import Movies from "../templates/movies";
 import Sidebar from "../templates/sidebar";
 
-const Home: NextPage = () => {
+type Props = {
+	movies: MovieData;
+	genreList: Genres;
+	searchedMovies: MovieData;
+};
+const Home: NextPage<Props> = ({ movies, genreList, searchedMovies }) => {
 	const router = useRouter();
-	const { data, isLoading } = useQuery<Genres>("genres", () => getMovieData());
+
+	const { data, isLoading } = useQuery<Genres>("genres", getMovieData, {
+		initialData: genreList,
+	});
 
 	const { data: movieData } = useQuery<MovieData>(
 		["movies", router.query.genre],
 		() => getMovieByGenre(router.query.genre),
-		{
-			refetchOnWindowFocus: false,
-		}
+		{ initialData: movies, refetchOnWindowFocus: false }
 	);
 
 	const { data: searchResults } = useQuery<MovieData>(
 		["search", router.query.title],
 		() => searchMoviesByTitle(router.query.title),
-		{
-			refetchOnWindowFocus: false,
-		}
+		{ initialData: searchedMovies, refetchOnWindowFocus: false }
 	);
 
 	if (isLoading) {
@@ -41,12 +45,16 @@ const Home: NextPage = () => {
 
 export default Home;
 
-export const getServerSideProps = async () => {
-	const queryClient = new QueryClient();
-	await queryClient.prefetchQuery<MovieData>("genres", getMovieData);
+export const getServerSideProps = async (context: any) => {
+	const { genre, title } = context?.query;
+	const movies = await getMovieByGenre(genre);
+	const genreList = await getMovieData();
+	const searchedMovies = await searchMoviesByTitle(title ? title : " ");
 	return {
 		props: {
-			dehydratedState: dehydrate(queryClient),
+			movies,
+			genreList,
+			searchedMovies,
 		},
 	};
 };
